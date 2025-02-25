e314883360d3a27881227d66d89241019c4dd979
Manikanta Reddy <122620649+manikanta9550@users.noreply.github.com>
2025-02-25 23:22:16 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -19,18 +19,17 @@ public class Pattern{
         }
         s += 4; 
         count -= 2;
-        while (s <= n + (n - 1)) {
-            for (int i = 1; i <= s; i++) {
-                System.out.print(count + " ");
-            }
-            System.out.println();
-            s += 2;
+        while (s <= n + (n - 1)) {                                        //pattern//
+            for (int i = 1; i <= s; i++) {                               //for n =4  
+                System.out.print(count + " ");                                        // 1 1 1 1 1 1 1
+            }                                                                         // 2 2 2 2 2
+            System.out.println();                                                     // 3 3 3
+            s += 2;                                                                   // 4
+                                                                                      // 3 3 3
+                                                                                      // 2 2 2 2 2
+                                                                                      // 1 1 1 1 1 1 1
             count--;
         }
-  
-
-       
-
 
     }
 }
