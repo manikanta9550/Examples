2061c003847211301bdc175214b6366ac8923aab
Manikanta Reddy <122620649+manikanta9550@users.noreply.github.com>
2025-03-03 21:55:24 +0530

    Add files via upload

---
+++ Sockspairs.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Sockspairs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt(); 
+        for (int i = 0; i < t; i++) {
+            int n = scanner.nextInt(); 
+            System.out.println(n + 1);
+        }
+    }
+}
